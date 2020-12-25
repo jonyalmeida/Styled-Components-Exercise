630b0b6da6c2fd2302f69c1226a183bb5d748451
jon <jon@jons-MacBook-Pro.local>
2020-12-25 11:01:56 -0600

    add basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <h1 className='App-title'>Welcome to React</h1>
+            </header>
+            <p className='App-intro'>
+                To get started edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className='App-link'
+                href='https://reactjs.org'
+                target='_blank'
+                rel='noopener noreferrer'>
+                Learn React
+            </a>
+        </div>
+    );
 }
-
-export default App;
